2486910b20faff006db50c740a6ba714bdc74134
tieubao1501 <82809279+tieubao1501@users.noreply.github.com>
2021-04-19 17:30:15 +0700

    Add files via upload

---
+++ th2-4.py
@@ -0,0 +1,5 @@
+i=1
+for j in range(2,10):
+    print ("1:","i","j:",j)
+    print (i,"/",j)
+    print (i/j);
---
+++ th2-5.py
@@ -0,0 +1,4 @@
+n=int(input("enter anumber--->"));
+while n>=0:
+          print(n);
+          n=n-1;
